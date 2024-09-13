056c650521ad3b7a31c9ed55e427a293228ed309
echo "rilschib <119910880+rilschib@users.noreply.github.com>
2024-09-13 09:00:32 -0500

    Added ripression

---
+++ Ripression.py
@@ -0,0 +1,7 @@
+
+import os
+import sys
+import pathlib
+import shutil
+
+replace = {'he':'J', 'in':'Z','ob':'U','er':'L','re':'K','ou':'X','ed':'V','ee':'~','an':'|','om':'0','ut':'1','es':'_','or':'+'} 
